e33bbf42d22a50aad6ccd403a142a59ebda62f78
SpookOz <50608124+SpookOz@users.noreply.github.com>
2020-09-02 12:54:11 +1000

    V1

---
+++ List-Installed-Software.ps1
@@ -0,0 +1,21 @@
+﻿# ------------------------------------------------------------------------- #
+# Variables
+# ------------------------------------------------------------------------- #
+
+# Change $AppsReportPath to wherever you wish
+
+$Today = Get-Date
+$TodayFile = get-date -f yyyy-MM-dd
+$AppsReportPath = "$Env:SystemDrive\IT"
+$AppsReportFile = "$AppsReportPath" + "\" + "InstalledProgramsList-" + "$TodayFile" + ".csv"
+
+
+# ------------------------------------------------------------------------- #
+# Commands
+# ------------------------------------------------------------------------- #
+
+$list = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
+$list += Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*
+$list | Where-Object {$_.DisplayName -and $_.UninstallString } | Select-Object DisplayName, Publisher, DisplayVersion, InstallLocation, InstallDate, URLInfoAbout, UninstallString | Sort-Object Publisher,DisplayName | Export-Csv -delimiter "`t" -encoding "unicode" "$AppsReportFile" -NoTypeInformation
+
+exit $returnStateOK
